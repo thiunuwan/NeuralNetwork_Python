118d5399f1e506a80114d69fb7a3524af144be9e
Ravindu Thiunuwan <83197833+thiunuwan@users.noreply.github.com>
2024-11-15 15:02:31 +0530

    Enhance model to handle multiple input sets

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import numpy as np
 
 # Model layer of neurons (with numpy)
 
-inputs =[1.8,7.5,4.2,5] 
+inputs =[ [1.8,7.5,4.2,5] , [5.8,3.5,4.25,5.6] , [3.8,7.5,4,5] , [1.89,7.35,7.2,9.5] ]
 
 weight_list =[[0.8,1.5,0.2,1.4],[1.8,0.5,4.2,8.4],[0.1,1.2,3.2,0.4]]
 
@@ -13,7 +13,7 @@ output_layer = []
 
 # Weights (weight_list) are multiplied by inputs, producing the output for each neuron.
 # The biases are added to the result for each corresponding neuron.
-output_layer = np.dot(weight_list,inputs) + biases
+output_layer = np.dot(inputs,np.array(weight_list).T) + biases
 
 print("Output of  the  layer : " ,output_layer)
 
