9b0644ca8625b1889d3aa54fa74c99bcd0bbf7da
Ravindu Thiunuwan <83197833+thiunuwan@users.noreply.github.com>
2024-11-15 09:46:11 +0530

    Refactor layer of neurons with loops

--- main.py
+++ main.py
@@ -11,18 +11,21 @@ print("Output :" ,output)
 # Model layer of neurons
 
 inputs =[1.8,7.5,4.2,5] 
-weights1 =[0.8,1.5,0.2,1.4]
-weights2 =[1.8,0.5,4.2,8.4]
-weights3 =[0.1,1.2,3.2,0.4]
-bias1 = 2
-bias2 = 3
-bias3 = -1
 
-output_lyr = [inputs[0]*weights1[0]+ inputs[1]*weights1[1]+ inputs[2]*weights1[2]+ inputs[3]*weights1[3]+bias1 ,
-          inputs[0]*weights2[0]+ inputs[1]*weights2[1]+ inputs[2]*weights2[2]+ inputs[3]*weights2[3]+bias2 ,
-          inputs[0]*weights3[0]+ inputs[1]*weights3[1]+ inputs[2]*weights3[2]+ inputs[3]*weights3[3]+bias3 ]
+weight_list =[[0.8,1.5,0.2,1.4],[1.8,0.5,4.2,8.4],[0.1,1.2,3.2,0.4]]
 
-print("Output of  the  layer : " ,output_lyr)
+biases = [2,3,-1]
+
+output_layer = []
+
+for neuron_weights,neuron_bias in zip (weight_list,biases):
+    neuron_output=0
+    for n_input,n_weight in zip (inputs,neuron_weights):
+        neuron_output= neuron_output + (n_input*n_weight)
+    neuron_output = neuron_output +  neuron_bias
+    output_layer.append(neuron_output)
+
+print("Output of  the  layer : " ,output_layer)
 
 
 
