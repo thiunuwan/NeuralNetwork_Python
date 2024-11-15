e791bb9463b0c0f3ec5f2f9342e6fad1afbbf01e
Ravindu Thiunuwan <83197833+thiunuwan@users.noreply.github.com>
2024-11-15 15:30:22 +0530

    Refactored code to use Layer_Dense class

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
 import numpy as np
 
-
 # Model layer of neurons (with numpy)
 
-inputs =[ [1.8,7.5,4.2,5] , [5.8,3.5,4.25,5.6] , [3.8,7.5,4,5] , [1.89,7.35,7.2,9.5] ]
+X =[ [1.8,7.5,4.2,5] , [5.8,3.5,4.25,5.6] , [3.8,7.5,4,5] , [1.89,7.35,7.2,9.5] ]
 
-weight_list_lyr1 =[[0.8,1.5,0.2,1.4],[1.8,0.5,4.2,8.4],[0.1,1.2,3.2,0.4]]
+class Layer_Dense:
+    def __init__(self,n_inputs,n_neurons): 
+        self.weights=  0.10 * np.random.randn(n_inputs,n_neurons)
+        self.biases = np.zeros((1,n_neurons))
+    def forward(self,inputs):
+        self.output = np.dot(inputs,self.weights) + self.biases
 
-weight_list_lyr1 =[[1.8,2.5,3.2,4.4],[2.8,1.5,3.2,1.4],[1.1,1.9,6.2,2.4]]
+layer1 = Layer_Dense(4,5)
+layer2 = Layer_Dense(5,2)
 
-biases1 = [2,3,-1]
-biases2 = [1,4,-2]
+layer1.forward(X)
+print("Output of  the  layer 1 : " ,layer1.output)
 
-# Weights (weight_list) are multiplied by inputs, producing the output for each neuron.
-# The biases are added to the result for each corresponding neuron.
-layer1_output = np.dot(inputs,np.array(weight_list_lyr1).T) + biases1
-layer2_output = np.dot(inputs,np.array(weight_list_lyr1).T) + biases2
-
-print("Output of  the  layer 1 : " ,layer1_output)
-print("-----------------------------------------")
-print("Output of  the  layer 2: " ,layer2_output)
+layer2.forward(layer1.output)
+print("Output of  the  layer 2 : " ,layer2.output)
 
 
 
