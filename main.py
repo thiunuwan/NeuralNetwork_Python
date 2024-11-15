67c093809bb057fd05879ca6bd484f2a54fd9c1b
Ravindu Thiunuwan <83197833+thiunuwan@users.noreply.github.com>
2024-11-15 10:40:05 +0530

    Update layer calculation using NumPy dot product

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
-# import numpy as np
+import numpy as np
 
 
-# # Model layer of neurons (with numpy)
+# Model layer of neurons (with numpy)
 
-# inputs =[1.8,7.5,4.2,5] 
+inputs =[1.8,7.5,4.2,5] 
 
-# weight_list =[[0.8,1.5,0.2,1.4],[1.8,0.5,4.2,8.4],[0.1,1.2,3.2,0.4]]
+weight_list =[[0.8,1.5,0.2,1.4],[1.8,0.5,4.2,8.4],[0.1,1.2,3.2,0.4]]
 
-# biases = [2,3,-1]
+biases = [2,3,-1]
 
-# output_layer = []
+output_layer = []
 
-# for neuron_weights,neuron_bias in zip (weight_list,biases):
-#     neuron_output=0
-#     for n_input,n_weight in zip (inputs,neuron_weights):
-#         neuron_output= neuron_output + (n_input*n_weight)
-#     neuron_output = neuron_output +  neuron_bias
-#     output_layer.append(neuron_output)
+# Weights (weight_list) are multiplied by inputs, producing the output for each neuron.
+# The biases are added to the result for each corresponding neuron.
+output_layer = np.dot(weight_list,inputs) + biases
 
-# print("Output of  the  layer : " ,output_layer)
+print("Output of  the  layer : " ,output_layer)
 
 
 
