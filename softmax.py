555fcc3b59e65bdabea71d39132599dd0200eb4c
Ravindu Thiunuwan <83197833+thiunuwan@users.noreply.github.com>
2024-11-16 00:04:27 +0530

    Add batch processing for softmax normalization

--- softmax.py
+++ softmax.py
@@ -1,12 +1,16 @@
 import numpy as np
 
-inputs = [0,2,2.2,-10,5,10]
-exp_values = np.exp(inputs)
+layer_inputs =[ [0,2,2.2,-10,5,10],
+          [1,2,2,-15,8,12],
+          [0,2,-2,8,5,6] ]
 
-print("Input data : ", inputs)
+exp_values = np.exp(layer_inputs)
+
+print("Input data : ", layer_inputs )
 print("exp values: ", exp_values)
 
-norm_values= exp_values/np.sum(exp_values)
+
+norm_values= exp_values/np.sum(exp_values, axis=1,keepdims=True)
 
 print("normalize values : ", norm_values)
 print("sum of normalize values : ", sum(norm_values))
