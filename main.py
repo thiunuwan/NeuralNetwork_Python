a2f45ab77bed0ba01cd1af12a88d684c430c1505
Ravindu Thiunuwan <83197833+thiunuwan@users.noreply.github.com>
2024-11-15 15:08:09 +0530

    Add second layer

--- main.py
+++ main.py
@@ -5,17 +5,21 @@ import numpy as np
 
 inputs =[ [1.8,7.5,4.2,5] , [5.8,3.5,4.25,5.6] , [3.8,7.5,4,5] , [1.89,7.35,7.2,9.5] ]
 
-weight_list =[[0.8,1.5,0.2,1.4],[1.8,0.5,4.2,8.4],[0.1,1.2,3.2,0.4]]
+weight_list_lyr1 =[[0.8,1.5,0.2,1.4],[1.8,0.5,4.2,8.4],[0.1,1.2,3.2,0.4]]
 
-biases = [2,3,-1]
+weight_list_lyr1 =[[1.8,2.5,3.2,4.4],[2.8,1.5,3.2,1.4],[1.1,1.9,6.2,2.4]]
 
-output_layer = []
+biases1 = [2,3,-1]
+biases2 = [1,4,-2]
 
 # Weights (weight_list) are multiplied by inputs, producing the output for each neuron.
 # The biases are added to the result for each corresponding neuron.
-output_layer = np.dot(inputs,np.array(weight_list).T) + biases
+layer1_output = np.dot(inputs,np.array(weight_list_lyr1).T) + biases1
+layer2_output = np.dot(inputs,np.array(weight_list_lyr1).T) + biases2
 
-print("Output of  the  layer : " ,output_layer)
+print("Output of  the  layer 1 : " ,layer1_output)
+print("-----------------------------------------")
+print("Output of  the  layer 2: " ,layer2_output)
 
 
 
