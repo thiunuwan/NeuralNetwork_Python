998bfcf844cc81fd61031ac7f636f708c57499ec
Ravindu Thiunuwan <83197833+thiunuwan@users.noreply.github.com>
2024-11-15 23:30:19 +0530

    Softmax Activation_numpy

--- softmax.py
+++ softmax.py
@@ -1,23 +1,12 @@
-import math
+import numpy as np
 
 inputs = [0,2,2.2,-10,5,10]
-exp_values = []
-
-E=math.e
-
-for i in inputs:
-    exp_values.append(E**i)
+exp_values = np.exp(inputs)
 
 print("Input data : ", inputs)
 print("exp values: ", exp_values)
 
-# calculate the sum of exp values
-norm_base =  sum(exp_values)
-
-norm_values= []
-
-for val in exp_values:
-    norm_values.append(val/norm_base)
+norm_values= exp_values/np.sum(exp_values)
 
 print("normalize values : ", norm_values)
 print("sum of normalize values : ", sum(norm_values))
